20ae9da9133c57dbbf2d06d05015a2018d45b0d3
zhongfu <me@zhongfu.li>
2022-11-17 15:25:25 +0800

    test: ERC20 test suite, etc (#18)
    * chore: update package{,-lock}.json
    
    * test: split out setup function
    
    * test: ERC20TestSuite, plus ERC20 compliance test for Funnel
    
    * test: use `bound` instead of `vm.assume` for input range checks
    
    Co-authored-by: zlace0x <zlace0x@gmail.com>

---
+++ test/FunnelERC20.t.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import "forge-std/Test.sol";
+import "openzeppelin-contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
+import {IERC20Metadata} from "openzeppelin-contracts/interfaces/IERC20Metadata.sol";
+import "../src/Funnel.sol";
+import {TestSetup, ERC20TestBase, ERC20TestSuite} from "./ERC20TestSuite.sol";
+import "../src/mocks/MockSpenderReceiver.sol";
+
+contract FunnelERC20Test is ERC20TestSuite {
+    event TransferReceived(address operator, address from, uint256 value);
+    event RenewableApprovalReceived(
+        address owner,
+        uint256 value,
+        uint256 recoveryRate
+    );
+
+    ERC20PresetMinterPauser baseToken;
+    Funnel funnel;
+
+    address minter;
+
+    function mintTokens(address to, uint256 amount)
+        public
+        virtual
+        override
+        returns (bool success)
+    {
+        vm.prank(minter);
+        baseToken.mint(to, amount);
+    }
+
+    function setUp() public override {
+        TestSetup.setUp();
+
+        minter = address(0x0123456789012345678901234567890123456789);
+
+        vm.prank(minter);
+        baseToken = new ERC20PresetMinterPauser("Test Base Token", "TEST");
+
+        funnel = new Funnel();
+        funnel.initialize(baseToken);
+        token = funnel;
+
+        vm.prank(user1);
+        baseToken.approve(address(funnel), type(uint256).max);
+        vm.prank(user2);
+        baseToken.approve(address(funnel), type(uint256).max);
+        vm.prank(user3);
+        baseToken.approve(address(funnel), type(uint256).max);
+
+        ERC20TestBase.setUp();
+    }
+
+    // not applicable for us
+    function testBalanceOfReflectsSlot(uint256 amount) public override {}
+}
